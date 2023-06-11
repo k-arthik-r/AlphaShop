82311b33be93ec4cb341167c4a98b942068d3b21
Karthik R <karthikclgid@gmail.com>
2023-06-11 13:40:48 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav className="bg-gray-900 py-4 px-6 flex justify-between items-center">
       <div className="text-white text-xl font-bold">Alpha Store</div>
       <div className="text-gray-300 flex items-center">
-        <span className='font-bold'>Balance:&nbsp;</span> <span className='text-green-500 font-bold'>787</span>&nbsp;Alpha coins
+        <span className='font-bold'>Balance:&nbsp;</span> <span className='text-green-500 font-bold'>786</span>&nbsp;Alpha coins
         <button className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handlelogout}>
           Logout
         </button>
