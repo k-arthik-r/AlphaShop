42740b19462a57f9fcfb4db2e48949fc22cfceca
Karthik R <karthikclgid@gmail.com>
2023-07-10 18:10:31 +0530

    projec commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   }
   return (
     <nav className="bg-gray-900 py-4 px-6 flex justify-between items-center">
-      <div className="text-white text-xl font-bold">Alpha Store</div>
+      <div className="text-white text-xl font-bold">Eco Store</div>
       <div className="text-gray-300 flex items-center">
         <span className='font-bold'>Balance:&nbsp;</span> <span className='text-green-500 font-bold'>786</span>&nbsp;Eco coins
         <button className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handlelogout}>
